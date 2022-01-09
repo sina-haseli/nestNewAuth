fd47d325afb0dea3a25ce6f94b0479ae1e79198f
Sina <sina.haseli@gmail.com>
2022-01-09 13:59:53 +0330

    init

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,36 +1,3 @@
-// import { PassportStrategy } from '@nestjs/passport';
-// import { passportJwtSecret } from 'jwks-rsa';
-// import { Strategy, ExtractJwt, VerifiedCallback } from 'passport-jwt';
-// import { Injectable, UnauthorizedException } from '@nestjs/common';
-//
-// @Injectable()
-// export class JwtStrategy extends PassportStrategy(Strategy) {
-//   constructor() {
-//     super({
-//       secretOrKeyProvider: passportJwtSecret({
-//         cache: true,
-//         rateLimit: true,
-//         jwksRequestsPerMinute: 5,
-//         jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-//       }),
-//
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // 1
-//       audience: 'http://localhost:1337',
-//       issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-//     });
-//   }
-//
-//   validate(payload: any, done: VerifiedCallback) {
-//     if (!payload) {
-//       done(new UnauthorizedException(), false); // 2
-//     }
-//
-//     return done(null, payload);
-//   }
-// }
-
-// --------------------------------------------------
-
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
